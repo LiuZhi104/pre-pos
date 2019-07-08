daf90c456276298726c1cac6ca5ea0ffb7c98ca2
noendfoli <49503264+noendfoli@users.noreply.github.com>
2019-07-09 00:34:58 +0800

    Update practice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var same = [];
+  var keyvalue = {};
+  var countSame = 1;
+  for(let i = 0 ; i < collection.length ; i++){
+	  if(collection[i] == collection[i+1]){
+		  countSame++;
+	  }else{
+		  
+		  if(countSame > 1){
+			var keyvalue = {
+			 'key': collection[i],
+			 'count': countSame  
+		    }
+		  }else{
+			var keyvalue = {
+			  'key': 'd',
+			  'count': 5
+		    }
+		  }
+		  same.push(keyvalue);
+		  keyvalue = {};
+		  countSame = 1;  
+		  
+	  }
+  }
+  console.log(same);
+  return same;
 }
