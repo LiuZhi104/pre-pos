7a48797fe859df0e4b8524f65675b8a0d6452da9
noendfoli <49503264+noendfoli@users.noreply.github.com>
2019-07-09 00:32:38 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var same =[];
+  var arrayb = collectionB[0];
+  for(let i =0 ;i<collectionA.length;i++){
+	  for(let j = 0 ; j < arrayb.length ; j++){
+        if(collectionA[i] == arrayb[j] ){
+           same.push(collectionA[i]);
+        }
+    }
+  }
+  return same;
 }
