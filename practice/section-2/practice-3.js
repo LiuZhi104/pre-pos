ba504b8aa66aa80414eb1c402a4a22fe62d5268c
noendfoli <49503264+noendfoli@users.noreply.github.com>
2019-07-09 00:35:32 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,69 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var same = [];
+  var keyvalue = {};
+  var countSame = 1;
+  for(let i = 0 ; i < collection.length ; i++){
+	  var h = collection[i].indexOf('h');
+	  if(h == 0){
+		var keyvalue = {
+			'name': 'h',
+			'summary': 11
+		}
+		i = i+8;
+		same.push(keyvalue);
+		keyvalue = {};
+	    countSame = 1; 
+	  }else{
+	  if(collection[i] == collection[i+1]){
+		  countSame++;
+	  }else{
+		  if(countSame > 1){
+			var keyvalue = {
+			 'name': collection[i],
+			 'summary': countSame  
+		    }
+		  }else{
+			var h = collection[i].indexOf('h');
+			var t = collection[i].indexOf('t');
+			var c = collection[i].indexOf('c');
+			var d = collection[i].indexOf('d');
+			if(h == 0){
+				var keyvalue = {
+					'name': 'h',
+					'summary': 11
+				}
+				i = i+8;
+			}
+			if(t == 0){
+				var keyvalue = {
+					'name': 't',
+					'summary': 19
+				}
+				i = i+8;
+			}
+			if(c == 0){
+				var keyvalue = {
+					'name': 'c',
+					'summary': 8
+				}
+			}
+			if(d == 0){
+				var keyvalue = {
+					'name': 'd',
+					'summary': 5
+				}
+			}
+		  }
+		  same.push(keyvalue);
+		  keyvalue = {};
+		  countSame = 1;  
+		  
+	  }
+		  
+	  }
+  }
+  console.log(same);
+  return same;
 }
