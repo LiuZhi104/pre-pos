d5ef81299fb50f6bbb0e450c09a93ff51bae9703
noendfoli <49503264+noendfoli@users.noreply.github.com>
2019-07-09 00:37:26 +0800

    Update practice-3.js

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,28 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var same = [];
+  var arrayB = objectB.value;
+  var countSame = 1;
+  for(let i = 0 ; i < collectionA.length ; i++){
+	  if(collectionA[i] == collectionA[i+1]){
+		  countSame++;
+	  }else{
+		  var keyvalue = {
+			'key': collectionA[i],
+			'count': countSame  
+		  }
+		  same.push(keyvalue);
+		  keyvalue = {};
+		  countSame = 1;
+	  }
+  }
+  for(let j = 0 ; j < same.length ; j++){
+	  for(let k = 0 ; k < arrayB.length ; k++){
+		  if(same[j].key == arrayB[k]){
+			  same[j].count =  same[j].count - Math.floor(same[j].count/3);
+		  }
+	  }
+  }
+  return same;
 }
